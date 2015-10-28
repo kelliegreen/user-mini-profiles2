ef76b61e74a2e8f90fdfff4307e19a436f51e49e
Kellie Green <kelliegreen10690@gmail.com>
2015-10-28 16:45:38 -0600

    fixed bug

--- service.js
+++ service.js
@@ -1,10 +1,13 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
-  this.getUsers = function() {
+app.service('mainService', function ($http) {
+
+  this.getUsers = function (pageNum) {
     return $http({
       method: 'GET',
-      url: 'http://reqres.in/api/users?page=1'
+      url: 'http://reqres.in/api/users?page=' + pageNum
+    }).then(function (response) {
+      return response.data;
     });
   };
-}); 
+});
